4e40e84a93080640c5ba75c0b3a818d14ccc0bd1
Leonardo Cerquerira <ljesuscerqueira@gmail.com>
2026-04-28 14:15:38 -0300

    Adicionando novos projetos JavaScript

---
+++ JavaScript/aulas práticas/functions.js
@@ -0,0 +1,126 @@
+window.onload = function(){
+
+    
+    
+    //var nome ='';
+    // === -> Além de ser igual a variável tem que ser do mesmo tipo!
+    // == -> Apenas o valor precisa ser igual
+    // != -> Diferente
+    // !== -> Diferente valor e tipo.
+    // >=
+    // <=
+    // > 
+    // <
+    //var numero = 10;
+    //if(numero !== '10'){
+    //    console.log("verdade");
+    //}
+    /*
+    var repeticao = 0;
+    var nome = ''
+    */
+    // While -> Precisamos de uma variável inicial
+    /*
+    while(nome == 'Guilherme'){
+        console.log("Olá Mundo");
+        nome = 'João';
+    }
+
+    console.log("Cheguei até o final!");
+    */
+    /*
+    for(var numero = 0; numero < 10; numero++){
+        console.log("Olá mundo!");
+    }
+    */
+
+    /*
+    do{ 
+       console.log("Olá mundo");
+    }while(nome == 'Guilherme');
+    */
+
+
+        /*
+        var variavel = function(){
+            console.log("Minha funcao através da variável");
+        };
+        */
+        /*
+        function teste(num1,num2){
+            return num1+num2;
+
+        }
+
+        var  variavel = teste(10,20);
+
+        console.log(variavel);
+        */
+         
+        /*
+         var obj = {'nome': 'Guilherme', 'idade':23,};
+
+         obj.func = function(){
+            console.log('Minha funcao');
+         }
+
+         obj.func();
+         */
+ 
+         /*
+         var variavel = [];
+
+         variavel[0] = [];
+
+         variavel [0][2] = 'Guilherme';
+
+         console.log(variavel[0][2]);
+        */
+
+        /*
+        var variavel = [];
+
+        variavel.obj = {};
+
+        variavel.obj.nome = 'Guilherme';
+
+        console.log(variavel.obj.nome);
+        */
+       
+        var n = 0;
+        //continue
+        //break
+
+        /*
+        for(var i = 0; i <= 10; i++){
+
+            if (i == 2) {
+                break;
+            }
+            console.log(i);
+        }
+      
+       while (n < 10) {
+        
+        if (n == 5) {
+            break
+        }
+        console.log(n);
+        n++;    
+       }
+       */
+
+       /*
+       var nome = "Guilherme";
+       switch(nome)
+       {
+        case "João":
+          //Posso fazer a ação que ru quiser.
+        break;
+        case "Guilherme":
+            alert("Guilherme");
+        break;
+       }
+        */
+
+}
