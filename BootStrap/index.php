41ad693929eee3e96d9c86b11857c90f56d1f504
Leonardo Cerquerira <ljesuscerqueira@gmail.com>
2026-07-04 00:02:34 -0300

    Adicionando projetos Bootstrap, Flexbox e Projeto06

---
+++ BootStrap/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BootStrap</title>
+    <style type="text/css">
+        *{
+            margin: 0;
+            padding:0;
+            box-sizing:border-box;
+        }
+        .parent{
+            margin:0 auto;
+            max-width:900px;
+        }
+        .box1{
+            float: left;
+            width:50%;
+            height:500px;
+            background: red;
+        }
+
+        .box2{
+            float: left;
+            width:50%;
+            height:200px;
+            background: orange;  
+        }
+    </style>
+</head>
+<body>
+
+   <div class="parent">
+    <div class="box1"></div>
+    <div class="box2"></div>
+    <div class="box2"></div>
+   </div><!--parent
+    
+</body>
+</html>
